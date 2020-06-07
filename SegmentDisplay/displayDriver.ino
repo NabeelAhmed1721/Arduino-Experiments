8f5d36e8f06977d1af5c9deab12899b25f765eab
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2020-06-07 00:04:26 -0400

    incrementer

--- SegmentDisplay/displayDriver.ino
+++ SegmentDisplay/displayDriver.ino
@@ -1,22 +1,3 @@
-int pins[] = {13, 12, 11, 10, 9, 8, 7}; // sorted from A - G for model 5641AS
-
-// int pA = 13;
-
-void setup() {
-  for (int x = 0; x < 7; x++ ) {
-    pinMode(pins[x], OUTPUT);
-  }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  for (int x = 0; x <= 9; x++ ) {
-    createDigit(x);
-    delay(250);
-    destroyAllDigits();
-  }
-}
-
 void createDigit(int number) {
   if (number >= 0 && number <= 9) {
     switch (number) {
